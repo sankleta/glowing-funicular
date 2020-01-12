01629ad42ed108d891a82ded8b86d216b27c2d0f
Tanya Kozel <sankleta@gmail.com>
2020-01-12 21:48:49 +0100

    4E-4G

--- baha/4E.py
+++ baha/4E.py
@@ -1,10 +1,12 @@
 n = int(input())
 trottoir = [input(), input()]
 
+
 def find_a_way(trottoir, n):
     for i in range(0, n):
         if trottoir[0][i] == "W" and trottoir[1][i] == "W":
             return "NO"
     return "YES"
+
 
 print(find_a_way(trottoir, n))
