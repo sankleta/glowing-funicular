333978a100a22c15ee69a7ac7c894f5d316ebe24
Tanya Kozel <sankleta@gmail.com>
2020-01-17 21:35:29 +0100

    5C recursive

--- baha/5C.py
+++ baha/5C.py
@@ -1,48 +1,43 @@
 # Дан ориентированный невзвешенный граф без кратных рёбер. Необходимо
 # определить, есть ли в нём циклы, и если есть, то вывести любой из них.
 
-from collections import defaultdict
-from queue import LifoQueue
+from collections import defaultdict, deque
 
 
 class Graph:
-    def __init__(self, directed=False):
+    def __init__(self):
         self._graph = defaultdict(set)
-        self._directed = directed
-        self._edges_count = 0
 
     def add(self, node1, node2):
         self._graph[node1].add(node2)
-        self._edges_count += 1
-        if not self._directed:
-            self._graph[node2].add(node1)
 
     def has_cycles(self):
-        if len(self._graph) == 1:
+        if len(self._graph) <= 1:
             return False
 
-        not_visited_nodes = set(self._graph.keys())
-        queue = LifoQueue()
-        used = []
+        visited = set()
+        for node in self._graph.keys():
+            if node not in visited and self.traverse(node, set(), visited):
+                return True
+        return False
 
-        while not_visited_nodes:
-            queue.put(not_visited_nodes.pop())
-            while not queue.empty():
-                current_node = queue.get()
-                used.append(current_node)
-                not_visited_nodes.discard(current_node)
-                adjacent_nodes = self._graph[current_node]
-                for i in adjacent_nodes:
-                    if i in used:
-                        return True
-                    elif i in not_visited_nodes:
-                        queue.put(i)
+    def traverse(self, current_node, cycle, visited):
+        if current_node not in self._graph:
+            return False
+        cycle.add(current_node)
+        visited.add(current_node)
+        for node in self._graph[current_node]:
+            if node in cycle:
+                return True
+            if node not in visited and self.traverse(node, cycle, visited):
+                return True
+        cycle.remove(current_node)
         return False
 
 
 nodes_no, edges_no = map(lambda x: int(x), input().split())
 
-graph = Graph(True)
+graph = Graph()
 
 for _ in range(0, edges_no):
     n1, n2 = map(lambda x: int(x), input().split())
