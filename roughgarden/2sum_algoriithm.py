6c1a81c8878c5316bb37e6c2dfc55af8b960f098
Tanya Kozel <sankleta@gmail.com>
2020-02-27 13:29:54 +0100

    add 2sum

---
+++ roughgarden/2sum_algoriithm.py
@@ -0,0 +1,41 @@
+import concurrent.futures
+import time
+
+integers = set()
+integers_l = []
+with open("algo1-programming_prob-2sum.txt", 'r') as f:
+    for line in f:
+        integers.add(int(line))
+
+integers_l = list(integers)
+
+
+def find_match(t):
+    for i in integers_l:
+        if i != (t - i) and (t - i) in integers:
+            return 1
+    return 0
+
+
+start = time.time()
+t_count = 0
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    for res in executor.map(find_match, range(0, 1000)):
+        t_count += res
+
+end = time.time()
+print(end - start)
+
+
+start = time.time()
+t_count = 0
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    for res in executor.map(find_match, range(0, 1000)):
+        t_count += res
+
+end = time.time()
+print(end - start)
+
+
+#98.73938703536987
+#191.09730315208435
