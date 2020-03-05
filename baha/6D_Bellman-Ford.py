1dfa961c36c928adc8558e6447bb0f9030783cf7
Tanya Kozel <sankleta@gmail.com>
2020-03-05 13:46:19 +0100

    rename

--- baha/6D_Bellman-Ford.py
+++ baha/6D_Bellman-Ford.py
@@ -7,32 +7,36 @@ class Graph:
         self._nodes_no = nodes_no
         self._edges_no = edges_no
 
-    def add(self, node1, node2, value):
-        self._graph[node1].append((node2, value))
+    def add(self, _from, to, value):
+        self._graph[_from].append((to, value))
 
     def bellman_ford(self, start):
+        # to_change - path weight changes during the current iteration
         to_change = {start: 0}
-        last_iteration_affected_nodes = [start]
-        calculated = [float("Inf")] * (self.nodes_no + 1)
-        i = 0
+
+        # affected_nodes - nodes that were affected during the previous iteration
+        affected_nodes = [start]
+
+        calculated = [float("Inf")] * (self._nodes_no + 1)
+        distance = 0
 
         while True:
-            for v in last_iteration_affected_nodes:
+            for v in affected_nodes:
                 for node, value in self._graph[v]:
                     min_value = min(to_change[node] if node in to_change else calculated[node], calculated[v] + value)
                     if min_value != calculated[node]:
-                        if i > (nodes_no - 1):
+                        if distance > (self._nodes_no - 1):
                             to_change[node] = -float("Inf")
                         else:
                             to_change[node] = min_value
             if to_change:
-                last_iteration_affected_nodes = list(to_change.keys())
-                for node in last_iteration_affected_nodes:
+                affected_nodes = list(to_change.keys())
+                for node in affected_nodes:
                     calculated[node] = to_change[node]
                 to_change = {}
             else:
                 break
-            i += 1
+            distance += 1
 
         return calculated[1:]
 
@@ -40,8 +44,8 @@ class Graph:
 nodes_no, edges_no, start = map(int, input().split())
 graph = Graph(nodes_no, edges_no)
 for i in range(0, edges_no):
-    node1, node2, value = map(int, input().split())
-    graph.add(node1, node2, value)
+    _from, to, value = map(int, input().split())
+    graph.add(_from, to, value)
 
 for i in graph.bellman_ford(start):
     if i == -float('Inf'):
