61ba52c9e41d81a4e7525bf88f9f39200ee78087
Tanya Kozel <sankleta@gmail.com>
2020-03-08 03:16:03 +0100

    add Floyd-Warshall

--- roughgarden/Floyd-Warshall.py
+++ roughgarden/Floyd-Warshall.py
@@ -10,3 +10,31 @@
 # If each of the three graphs has a negative-cost cycle, then enter "NULL" in the box
 # below. If one or more of the graphs have no negative-cost cycles, then enter
 # the smallest of the lengths of their shortest shortest paths.
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, nodes_no, edges_no):
+        self._graph = defaultdict(list)
+        self._nodes_no = nodes_no
+        self._edges_no = edges_no
+
+    def add(self, _from, to, value):
+        self._graph[_from].append((to, value))
+
+    def floyd_warshall(self):
+        paths = [[float("Inf")] * self._nodes_no for i in range(self._nodes_no)]
+        for i in range(self._nodes_no):
+            for j in range(self._nodes_no):
+                pass
+
+
+with open("g1.txt", 'r') as f:
+    nodes_no, edges_no = map(lambda x: int(x), next(f).split())
+    graph = Graph(nodes_no, edges_no)
+
+    for line in f:
+        _from, to, value = map(lambda x: int(x), line.split())
+        graph.add(_from, to, value)
+
+    print(graph.floyd_warshall())
