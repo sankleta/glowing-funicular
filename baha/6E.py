53e8334bb2a83468d9da832e5c43cef8f1ef7e28
Tanya Kozel <sankleta@gmail.com>
2020-03-12 01:58:44 +0100

    optimize 6E

--- baha/6E.py
+++ baha/6E.py
@@ -1,30 +1,20 @@
 from collections import defaultdict
 
+inf = 10000 * 2001
+
 
 class Graph:
     def __init__(self, nodes_no, edges_no, start, stop):
-        self._graph = defaultdict(dict)
+        self._graph = defaultdict(list)
         self._nodes_no = nodes_no
         self._edges_no = edges_no
         self._start = start
         self._stop = stop
-        self._stop_in_degree = 0
 
     def add(self, _from, to, value):
-        if to == self._stop:
-            self._stop_in_degree += 1
-
-        if _from in self._graph and to in self._graph[_from]:
-            if value > self._graph[_from][to]:
-                self._graph[_from][to] = value
-        else:
-            self._graph[_from][to] = value
+        self._graph[_from].append((to, value))
 
     def bellman_ford(self):
-        if self._start not in self._graph:
-            return -float("Inf")
-        if self._stop_in_degree == 0:
-            return -float("Inf")
 
         # to_change - path weight changes during the current iteration
         to_change = {self._start: 0}
@@ -32,31 +22,30 @@ class Graph:
         # affected_nodes - nodes that were affected during the previous iteration
         affected_nodes = [self._start]
 
-        calculated = [-float("Inf")] * (self._nodes_no + 1)
+        calculated = [-inf] * (self._nodes_no + 1)
         distance = 0
 
         while True:
             for v in affected_nodes:
-                for node, value in self._graph[v].items():
+                for node, value in self._graph[v]:
                     max_value = max(to_change[node] if node in to_change else calculated[node], calculated[v] + value)
                     if max_value != calculated[node]:
                         if distance > (self._nodes_no - 1):
-                            to_change[node] = float("Inf")
+                            if node == self._stop:
+                                return inf
+                            to_change[node] = inf
                         else:
                             to_change[node] = max_value
             if to_change:
                 affected_nodes = list(to_change.keys())
                 for node in affected_nodes:
                     calculated[node] = to_change[node]
-                    if node == self._stop and calculated[node] == float("Inf"):
-                        return calculated[-1]
                 to_change = {}
             else:
                 break
             distance += 1
 
         return calculated[-1]
-
 
 nodes_no, edges_no = map(int, input().split())
 graph = Graph(nodes_no, edges_no, 1, nodes_no)
@@ -66,9 +55,9 @@ for i in range(0, edges_no):
 
 res = graph.bellman_ford()
 
-if res == -float("Inf"):
+if res == -inf:
     print(":(")
-elif res == float("Inf"):
+elif res >= inf:
     print(":)")
 else:
     print(res)
--- roughgarden/Johnson.py
+++ roughgarden/Johnson.py
@@ -1,30 +1,72 @@
+from collections import defaultdict
+from heapq import heappop, heappush
+
+
 class Graph:
-    def __init__(self, nodes_no):
+    def __init__(self, nodes_no, edges_no):
+        self._graph = defaultdict(list)
         self._nodes_no = nodes_no
-        self._paths = [[float("Inf")] * self._nodes_no for i in range(self._nodes_no)]
-        self._has_negative_cycle = False
-        self._shortest_length = 0
-        for i in range(self._nodes_no):
-            self._paths[i][i] = 0
+        self._edges_no = edges_no
+        self._visited = dict()
 
     def add(self, _from, to, value):
-        if _from == to and to < 0:
-            self._has_negative_cycle = True
+        self._graph[_from].append((to, value))
 
-        if self._paths[_from - 1][to - 1] > value:
-            self._paths[_from - 1][to - 1] = value
+    def bellman_ford(self, start):
+        # to_change - path weight changes during the current iteration
+        to_change = {start: 0}
 
-        if value < self._shortest_length:
-            self._shortest_length = value
+        # affected_nodes - nodes that were affected during the previous iteration
+        affected_nodes = [start]
+
+        calculated = [float("Inf")] * (self._nodes_no + 1)
+        distance = 0
+
+        while True:
+            for v in affected_nodes:
+                for node, value in self._graph[v]:
+                    min_value = min(to_change[node] if node in to_change else calculated[node], calculated[v] + value)
+                    if min_value != calculated[node]:
+                        if distance > self._nodes_no:
+                            to_change[node] = -float("Inf")
+                        else:
+                            to_change[node] = min_value
+            if to_change:
+                affected_nodes = list(to_change.keys())
+                for node in affected_nodes:
+                    calculated[node] = to_change[node]
+                to_change = {}
+            else:
+                break
+            distance += 1
+
+        return calculated
+
+    def dijkstra(self, start_node):
+        heap = []
+        heappush(heap, (0, start_node))
+        while heap:
+            value, node = heappop(heap)
+            if node in self._visited:
+                continue
+            self._visited[node] = value
+            for i, j in self._graph[node]:
+                if i not in self._visited:
+                    heappush(heap, (value + j, i))
+
+        return self._visited
 
     def johnson(self):
-        pass
+        for v in range(1, self._nodes_no + 1):
+            self.add(0, v, 0)
+
+        reweighting_coef = self.bellman_ford(0)
 
 
 def calculate_shortest_shortest_path(filename):
     with open(filename, 'r') as f:
         nodes_no, edges_no = map(lambda x: int(x), next(f).split())
-        graph = Graph(nodes_no)
+        graph = Graph(nodes_no, edges_no)
 
         for line in f:
             _from, to, value = map(lambda x: int(x), line.split())
