6cd3334172076ad9731276127681b43f0a5c7c46
Tanya Kozel <sankleta@gmail.com>
2020-03-13 02:42:03 +0100

    finish Johnson

--- roughgarden/Johnson.py
+++ roughgarden/Johnson.py
@@ -1,3 +1,16 @@
+# The first line indicates the number of vertices and edges, respectively.
+# Each subsequent line describes an edge (the first two numbers are its tail and head,
+# respectively) and its length (the third number).
+# NOTE: some of the edge lengths are negative.
+# NOTE: These graphs may or may not have negative-cost cycles.
+# Your task is to compute the "shortest shortest path".
+# Precisely, you must first identify which, if any, of the three graphs have no
+# negative cycles. For each such graph, you should compute all-pairs shortest paths
+# and remember the smallest one.
+# If each of the three graphs has a negative-cost cycle, then enter "NULL" in the box
+# below. If one or more of the graphs have no negative-cost cycles, then enter
+# the smallest of the lengths of their shortest shortest paths.
+
 from collections import defaultdict
 from heapq import heappop, heappush
 
@@ -7,7 +20,7 @@ class Graph:
         self._graph = defaultdict(list)
         self._nodes_no = nodes_no
         self._edges_no = edges_no
-        self._visited = dict()
+        self._has_cycles = False
 
     def add(self, _from, to, value):
         self._graph[_from].append((to, value))
@@ -28,7 +41,7 @@ class Graph:
                     min_value = min(to_change[node] if node in to_change else calculated[node], calculated[v] + value)
                     if min_value != calculated[node]:
                         if distance > self._nodes_no:
-                            to_change[node] = -float("Inf")
+                            return calculated, True
                         else:
                             to_change[node] = min_value
             if to_change:
@@ -40,27 +53,39 @@ class Graph:
                 break
             distance += 1
 
-        return calculated
+        return calculated, False
 
-    def dijkstra(self, start_node):
+    def dijkstra(self, start_node, reweighting_coef):
+        visited = dict()
         heap = []
         heappush(heap, (0, start_node))
         while heap:
             value, node = heappop(heap)
-            if node in self._visited:
+            if node in visited:
                 continue
-            self._visited[node] = value
+            visited[node] = value - reweighting_coef[start_node] + reweighting_coef[node]
             for i, j in self._graph[node]:
-                if i not in self._visited:
-                    heappush(heap, (value + j, i))
+                if i not in visited:
+                    heappush(heap, (value + j + reweighting_coef[node] - reweighting_coef[i], i))
 
-        return self._visited
+        return visited
 
     def johnson(self):
         for v in range(1, self._nodes_no + 1):
             self.add(0, v, 0)
 
-        reweighting_coef = self.bellman_ford(0)
+        reweighting_coef, has_cycles = self.bellman_ford(0)
+
+        if not has_cycles:
+            shortest_length = float("Inf")
+            for v in range(1, self._nodes_no + 1):
+                paths = self.dijkstra(v, reweighting_coef)
+                for i in paths:
+                    if paths[i] < shortest_length:
+                        shortest_length = paths[i]
+            return shortest_length
+        else:
+            return "NEGATIVE CYCLE"
 
 
 def calculate_shortest_shortest_path(filename):
