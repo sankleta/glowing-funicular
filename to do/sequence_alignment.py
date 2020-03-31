1467abbdb7a0ba8d39efa628c246aec3b7e0de64
Tanya Kozel <sankleta@gmail.com>
2020-03-31 22:53:01 +0200

    sequence_alignment.py

--- to do/sequence_alignment.py
+++ to do/sequence_alignment.py
@@ -1,1 +1,3 @@
 # Global sequence alignment using Needleman-Wunsch score
+# Take scores here https://bioinformaticshome.com/bioinformatics_tutorials/sequence_alignment/DNA_scoring_matrices.html
+# Use test data from here https://bioinformaticshome.com/online_software/align2sequences/nw2seqalign.html
